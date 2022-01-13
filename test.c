ff6fa19e54bdee74daba1025486cae4557025d3c
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-01-13 10:56:25 -0500

    renaming player to synth

--- test.c
+++ test.c
@@ -1,26 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 
 #include "log.h"
-#include "player.h"
+#include "synth.h"
 
 int main()
 {
     setup_log(STDOUT_FILENO);
     int pid = 0;
-    
+
     while (1)
-    {   
+    {
         char c[2] = "\0\0";
-        
+
         fgets(c, sizeof(c), stdin);
 
         if (c[0] == 'p')
         {
             printf("play\n");
-            pid = play("pluck", "C");
+            pid = play("sine", "C");
         }
         else if (c[0] == 's')
         {
