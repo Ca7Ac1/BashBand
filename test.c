9d7f496fea095973ba0c0e8f161457a9fd7080fd
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-01-12 19:39:01 -0500

    Test file for testing sound playing

---
+++ test.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h> 
+
+#include "log.h"
+#include "player.h"
+
+int main()
+{
+    setup_log(STDOUT_FILENO);
+    int pid = 0;
+    
+    while (1)
+    {   
+        char c[2] = "\0\0";
+        
+        fgets(c, sizeof(c), stdin);
+
+        if (c[0] == 'p')
+        {
+            printf("play\n");
+            pid = play("pluck", "C");
+        }
+        else if (c[0] == 's')
+        {
+            printf("%d, stop\n", pid);
+            stop(pid);
+        }
+
+        c[0] = 'u';
+    }
+
+    return 0;
+}
