972165ccea8ae8b116d8ceb384ddd205a15e3dfb
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-01-13 23:25:23 -0500

    name issue

--- networking.h
+++ networking.h
@@ -14,7 +14,7 @@ int server_setup(char *addr, char *port);
 int server_connect(int server_socket);
 int client_setup(char *addr, char *port);
 int read_connections(connections *c);
-connections *add_connections(connections *c, int descriptor, int id);
+connections *add_connection(connections *c, int descriptor, int id);
 connections *remove_connection(connections *c, int id);
 connections *free_connections(connections *c);
 
