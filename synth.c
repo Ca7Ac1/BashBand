b684c7f57a225bee8f7c60d7204bd052a695383a
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-01-17 01:59:17 -0500

    notes list

--- synth.c
+++ synth.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "synth.h"
 #include "log.h"
@@ -25,5 +26,62 @@ int play(char *instrument, char *note)
 
 void stop(int pid)
 {
-    kill(pid, SIGKILL);
+    err(kill(pid, SIGKILL));
+}
+
+
+notes *add_note(notes *n, char *instrument, char *note)
+{
+    notes *temp = malloc(sizeof(notes));
+    
+    strcpy(temp->note, note);
+    strcpy(temp->instrument, instrument);
+    temp->pid = play(instrument, note);
+    temp->next = n;
+    
+    return temp;
+}
+
+notes *remove_note(notes *n, int pid)
+{
+    if (n && n->pid == pid)
+    {
+        stop(pid);
+
+        notes *temp = n;
+        n = n->next;
+        free(temp);
+
+        return n;
+    }
+
+    while (n->next)
+    {
+        if (n->next->pid == pid)
+        {
+            stop(pid);
+            
+            notes *temp = n->next;
+            n->next = n->next->next;
+            free(temp);
+
+            return n;
+        }
+
+        n = n->next;
+    }
+
+    return n;
+}
+
+notes *free_notes(notes *n)
+{
+    while (n)
+    {
+        notes *temp = n;
+        n = n-> next;
+        free(temp);
+    }
+
+    return NULL;
 }
--- synth.h
+++ synth.h
@@ -4,7 +4,19 @@
 // instruments avaliable:
 // sine, square, triangle, sawtooth, trapezium, exp, [white]noise, tpdfnoise, pinknoise, brownnoise, pluck
 
+typedef struct notes
+{
+    int pid;
+    char *instrument;
+    char *note;
+    struct notes *next;
+} notes;
+
 int play(char *instrument, char *note);
 void stop(int pid);
+
+notes *add_note(notes *n, char *instrument, char *note);
+notes *remove_note(notes *n, int pid);
+notes *free_notes(notes *n);
 
 #endif
