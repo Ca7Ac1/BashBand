efe647f2cc1e2b8b636a6fc6b4952361c453cca4
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-01-20 21:46:57 -0500

    SDL input tests

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <SDL2/SDL.h>
 
 #include "log.h"
 #include "synth.h"
@@ -9,10 +10,46 @@
 
 int main()
 {
-    input();
+    setup_log(STDOUT_FILENO);
 
-    while (1) {}
-    // setup_log(STDOUT_FILENO);
+    SDL_Init(SDL_INIT_VIDEO);
+    // SDL_EnableUNICODE(1);
+
+    SDL_Window *win = SDL_CreateWindow("input", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Event key_event;
+
+    printf("%d\n", getpid());
+    while (1)
+    {
+        while (SDL_PollEvent(&key_event))
+        {
+            if (key_event.type == SDL_KEYDOWN)
+            {
+                SDL_KeyboardEvent *key = &key_event.key;
+                const char *keystroke = SDL_GetKeyName(key->keysym.sym);
+                printf("%s pressed\n", keystroke);
+
+                if (keystroke[0] == 'q')
+                {
+                    SDL_Quit();
+                    exit(0);
+                }
+            }
+            else if (key_event.type == SDL_KEYUP)
+            {
+                SDL_KeyboardEvent *key = &key_event.key;
+                const char *keystroke = SDL_GetKeyName(key->keysym.sym);
+                printf("%s released\n", keystroke);
+            }
+        }
+    }
+
+    SDL_Quit();
+    // input();
+
+    // while (1) {}
     // int pid = 0;
 
     // key *notes = setup_notes();
